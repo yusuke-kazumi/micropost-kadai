42319cf58f95d0ce4af2d86fcd7d25c7df80b045
yusuke.kazumi <yusuke.a.kazumi@rakuten.com>
2018-06-19 06:47:52 +0000

    follow, unfollow

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,15 @@ Route::get('/', 'MicropostsController@index');
 
 // omit
 
+
 Route::group(['middleware' => 'auth'], function () {
     Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+    Route::group(['prefix' => 'users/{id}'], function () {
+        Route::post('follow', 'UserFollowController@store')->name('user.follow');
+        Route::delete('unfollow', 'UserFollowController@destroy')->name('user.unfollow');
+        Route::get('followings', 'UsersController@followings')->name('users.followings');
+        Route::get('followers', 'UsersController@followers')->name('users.followers');
+    });
+
     Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
 });
