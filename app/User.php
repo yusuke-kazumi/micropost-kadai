75ecc42b24b88f1a645eb8f106372c0d5f671a4f
yusuke.kazumi <yusuke.a.kazumi@rakuten.com>
2018-06-20 01:33:28 +0000

    User.php change

--- app/User.php
+++ app/User.php
@@ -37,6 +37,11 @@ class User extends Authenticatable
         return $this->belongsToMany(User::class, 'user_follow', 'user_id', 'follow_id')->withTimestamps();
     }
 
+    public function followers()
+    {
+        return $this->belongsToMany(User::class, 'user_follow', 'follow_id', 'user_id')->withTimestamps();
+    }
+
 public function follow($userId)
 {
     // confirm if already following
