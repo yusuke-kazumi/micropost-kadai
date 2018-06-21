f366eeb107eaf6f743dc17e96607a6900a147439
yusuke.kazumi <yusuke.a.kazumi@rakuten.com>
2018-06-21 06:08:11 +0000

    test2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,7 +14,7 @@
    @else
     <div class="center jumbotron">
         <div class="text-center">
-            <h1>Welcome to the Microposts</h1>
+            <h1>Welcome to the Microposts2</h1>
             {!! link_to_route('signup.get', 'Sign up now!', null, ['class' => 'btn btn-lg btn-primary']) !!}
         </div>
     </div>
